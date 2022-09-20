cf8f3a9a85d074d7e90047d4cb3ee9f1f6de98e9
Moamin <moamin.salamah@gmail.com>
2022-09-20 10:35:07 +0200

    Updates

---
+++ js/about.03d05ab6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuejs_dashboard"]=self["webpackChunkvuejs_dashboard"]||[]).push([[443],{1272:function(s,u,n){n.r(u),n.d(u,{default:function(){return i}});var t=function(){var s=this;s._self._c;return s._m(0)},a=[function(){var s=this,u=s._self._c;return u("div",{staticClass:"about"},[u("h1",[s._v("This is an about page")])])}],e=n(1001),r={},l=(0,e.Z)(r,t,a,!1,null,null,null),i=l.exports}}]);
+//# sourceMappingURL=about.03d05ab6.js.map
